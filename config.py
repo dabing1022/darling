0ba62c7a3bb4074748672ee33bb914f1d3f1b928
zhaoweibing <zhaoweibing@zhuanzhuan.com>
2018-01-02 09:55:25 +0800

    update darling

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 # coding:utf-8
 
-DARLING = '猪仔仔'
+DARLING = '宝'
 TULING_KEY = 'ad6c2ada8e6d4509944b7d0ab289339c'
 TULING_URL = 'http://www.tuling123.com/openapi/api'
-YIZUER = '衣卒尔'
+MIKE = '[迈克]'
 FILE_HELPER = 'filehelper'
 MSG_TYPE = 'Type'
 MSG_TEXT = 'Text'
 MSG_FROM_USER_NAME = 'FromUserName'
+MSG_TO_USER_NAME = 'ToUserName'
+REMARK_NAME = 'RemarkName'
+TULING_AUTOREPLY_ON = False
--- darling.py
+++ darling.py
@@ -5,9 +5,12 @@ import requests
 import itchat
 from itchat.content import *
 from config import *
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime, date
 #from emailSender import *
 
 #email_sender = EmailSender()
+tuling_reply_on = False
 
 def get_response(msg):
     data = {
@@ -23,21 +26,29 @@ def get_response(msg):
 
 @itchat.msg_register([TEXT])
 def tuling_reply(msg):
+    global tuling_reply_on
+    reply = ""
     print(msg)
     content = msg[MSG_TEXT]
     userName = msg[MSG_FROM_USER_NAME]
+
+    print(msg[MSG_TO_USER_NAME])
+    if msg[MSG_TO_USER_NAME] == FILE_HELPER:
+        if re.search(r'tuling_reply_on', content):
+            print("tuling on")
+            tuling_reply_on = True
+        elif re.search(r'tuling_reply_off', content):
+            print("tuling off")
+            tuling_reply_on = False
+
     importantPattern = re.compile('重要')
     if (importantPattern.search(content)):
-        forwardMsg = msg[MSG_FROM_USER_NAME] + ":" + content
         itchat.send(content, FILE_HELPER)
-    defaultReply = '接受消息: ' + content
 
-    if (userName == DARLING):
-        # reply = YIZUER + defaultReply
-        reply = ""
+    if tuling_reply_on:
+        reply = MIKE + ":" + get_response(content)
     else:
-        # reply = YIZUER + ":" + get_response(content)
-        reply = ""
+        pass
 
     return reply
 
@@ -55,15 +66,24 @@ def download_files_fromPerson(msg):
     content = u'Recevie File：%s' % msg['FileName']
     email_sender.send_email(content, msg['FileName'])
 
+def job():
+    time_report = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(time_report)
+    itchat.send(time_report, FILE_HELPER)
 
 if __name__ == '__main__':
     # login
-    itchat.auto_login(enableCmdQR=2, hotReload=True)
+    itchat.auto_login(enableCmdQR=2, hotReload=False)
 
     # welcome
     WELCOME = u''
     WELCOME += u'Welcome, I am ChildhoodAndy AI Helper. You can call me Mike.\n'
     itchat.send(WELCOME, FILE_HELPER)
+
+    sched = BlockingScheduler()
+    # sched.add_job(job, 'interval', seconds=5)
+    # sched.add_job(job, 'date', run_date = datetime(2018, 1, 1, 16, 43, 0))
+    # sched.start()
 
     # run
     itchat.run()
